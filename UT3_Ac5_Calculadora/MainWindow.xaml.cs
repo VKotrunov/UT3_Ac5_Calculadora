fc613e3b0684756269cc2bf0c8ef1a8f7c209106
VKotrunov <57618856+VKotrunov@users.noreply.github.com>
2020-11-02 19:25:08 +0100

    Agregar archivos de proyecto.

---
+++ UT3_Ac5_Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace UT3_Ac5_Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textoSalidaTextBlock.Text += (sender as Button).Tag.ToString();
+        }
+    }
+}
